8f413aae1ee2f5b2c02bdfbfa1d012f8d64004e7
Elias Segura <eliasjsegura00@gmail.com>
2024-08-25 17:32:16 -0600

    cambios

--- client/src/components/ListTasks.jsx
+++ client/src/components/ListTasks.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import ModalComponent from "./ModalComponent"
 import axios from "axios"
-import { set } from "mongoose"
+import Swal from 'sweetalert2'
 
 const ListTasks = () => {
     const [ ModalIsOpen, SetModalIsOpen ] = useState(false)
@@ -19,14 +19,29 @@ const ListTasks = () => {
         axios.post('/tasks', values)
         .then((res)=>{
             console.log(res)
+            Swal.fire({
+                title: 'Exito!',
+                text: 'La tarea se agrego correctamente',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
             onRequestClose()
         })
         .catch((err)=>console.log(err))
     }
 
+    const [data, setData] = useState([])
+    useEffect(() =>{
+        axios.get('/tasks')
+        .then((res) => {
+            setData(res.data)
+        })
+        .catch((err) => console.log(err))
+    })
+
     return (
-        <div className="container">
-            <div className="card m-auto mt-5" style={{ width: 700 }}>
+        <div className="container-fluid">
+            <div className="card m-auto mt-5" style={{ width: 900 }}>
                 <div className="row">
                     <div className="cl-md-2 ms-3 mt-3">
                         <button onClick={openModal} className="btn btn-success">Agregar</button>
@@ -37,6 +52,33 @@ const ListTasks = () => {
                             setValues={setValues}
                             values={values}></ModalComponent>
                     </div>
+                </div>
+                <div className="row">
+                    <table className="table table-hovered">
+                        <thead className="thead-black">
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Titulo</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map((task) => {
+                                    return(<tr>
+                                        <td>{task.id}</td>
+                                        <td>{task.name}</td>
+                                        <td>{task.description}</td>
+                                        <td>
+                                            <a href="" role="button"><i class="bi bi-pencil-fill"></i></a>
+                                            <a href="" role="button"><i class="bi bi-trash"></i></a>
+                                        </td>
+                                    </tr>)
+                                })
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
